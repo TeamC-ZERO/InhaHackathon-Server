d442d737a43f38d2dda8c25e91b312f0ce06929c
sunwoong <sunwoong1122@gmail.com>
2023-07-15 04:46:55 +0900

    feat: 유통기한 마감 재료 조회 & 식재료 조회 API 기능 구현 #4

---
+++ src/main/java/org/inha/hackathon/refrigerator/repository/RefrigeratorRepository.java
@@ -0,0 +1,18 @@
+package org.inha.hackathon.refrigerator.repository;
+
+import org.inha.hackathon.refrigerator.entity.Ingredient;
+import org.inha.hackathon.refrigerator.entity.Refrigerator;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RefrigeratorRepository extends JpaRepository<Ingredient, Long> {
+    @Query("select r from Refrigerator r where r.user.id = :userId")
+    Optional<Refrigerator> findRefrigerator(@Param("userId") Long userId);
+
+    @Query("select i from Ingredient i join fetch i.ingredientMeta im where i.refrigerator.id = :refrigeratorId")
+    List<Ingredient> findIngredients(@Param("refrigeratorId") Long refrigeratorId);
+}
