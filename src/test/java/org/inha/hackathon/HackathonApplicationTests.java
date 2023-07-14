e7315158131705500ad898df73d9fcd8aa8a95d9
sunwoong <sunwoong1122@gmail.com>
2023-07-15 00:02:09 +0900

    init: 프로젝트 세팅

---
+++ src/test/java/org/inha/hackathon/HackathonApplicationTests.java
@@ -0,0 +1,11 @@
+package org.inha.hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
